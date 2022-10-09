ea9f4eee77809f034021d686cf5f9d6343443c90
ojoadekunle <ojoadekunle84@gmail.com>
2022-10-09 21:18:01 +0100

    Deployment: Flask

---
+++ chapter 5 - deployment/ping.py
@@ -0,0 +1,16 @@
+# importing python packages. 
+from Flask import Flask
+
+# Creating Flask instance.
+app = Flask()
+
+# Creating flask rourt with python decorator.
+@app.rourt('/ping',methods = ['GET'])
+
+# Defining ping function.
+def ping():
+    return 'PONG'
+
+if __name__ = '__main__':
+    app.run(debug = True, host='0.0.0.0',  port = 9695)
+   
