6463a3265e7544fcdcf895430fb0976b7e875d81
ojoadekunle <ojoadekunle84@gmail.com>
2022-10-09 20:45:18 +0100

    Deployment: Saving and Loading Model

--- chapter 5 - deployment/train.py
+++ chapter 5 - deployment/train.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # Importing packages
 import numpy as np
 import pandas as pd 
@@ -14,38 +8,22 @@ from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+# Importing pickle package.
+import pickle
 
-# In[2]:
+# Setting parameters.
 
+# Logistic regression parameter.
+c = 0.1
 
 # Load dataset.
 telco_data = pd.read_csv('data/telco_customer_churn.csv')
-
-# Checking dataframe.
-telco_data.head()
-
-
-# In[3]:
-
 
 # Cleaning up columns names and categorical variables. 
 columns = telco_data.columns.str.lower().str.replace(' ', '_')
 
 # changeing columns names 
 telco_data.columns = columns
-
-# Checking for changes made. 
-telco_data.columns
-
-
-# In[ ]:
-
-
-
-
-
-# In[4]:
-
 
 # Categorical Features. 
 categorical = ['customerid', 'gender', 'partner', 'dependents', 'phoneservice',
@@ -57,18 +35,6 @@ categorical = ['customerid', 'gender', 'partner', 'dependents', 'phoneservice',
 # Converting to lower case and removing space. 
 for c in categorical: 
     telco_data[c] = telco_data[c].str.lower().str.replace(' ', '_')
-    print(c)
-
-
-# In[5]:
-
-
-# Checking dataframe.
-telco_data.head().T
-
-
-# In[6]:
-
 
 # Converting totalcharge column to number. 
 telco_data['totalcharges'] = pd.to_numeric(telco_data['totalcharges'], errors='coerce')
@@ -76,22 +42,8 @@ telco_data['totalcharges'] = pd.to_numeric(telco_data['totalcharges'], errors='c
 # Filling null values with 0. 
 telco_data['totalcharges'] = telco_data['totalcharges'].fillna(0)
 
-# checking for null. 
-telco_data['totalcharges'].isna().sum()
-
-
-# In[7]:
-
-
 # converting churn to int
 telco_data['churn'] = (telco_data['churn']== 'yes').astype(int)
-
-# Checking churn features.
-telco_data.churn.value_counts()
-
-
-# In[8]:
-
 
 # Setting up data validation dataset. 
 telco_train, telco_test = train_test_split(telco_data, test_size = 0.20, random_state = 1)
@@ -107,16 +59,6 @@ y_test = telco_test['churn'].values
 del telco_train['churn']
 del telco_test['churn']
 
-
-# In[9]:
-
-
-telco_test
-
-
-# In[10]:
-
-
 # Defining a training function. 
 def train(df_train, df_y, c = 1.0):
     model = LogisticRegression(C= c , max_iter = 1000)
@@ -129,10 +71,6 @@ def train(df_train, df_y, c = 1.0):
     
     return model, dv
 
-
-# In[11]:
-
-
 # Defining prediction function. 
 def predict(df_val, model, dv): 
     dict_val = df_val.to_dict(orient = 'records')
@@ -141,19 +79,6 @@ def predict(df_val, model, dv):
     y_pred = model.predict_proba(x_val)[:, 1]
     
     return y_pred
-
-
-# In[12]:
-
-
-# Setting parameters.
-
-# Logistic regression parameter.
-c = 0.1
-
-
-# In[13]:
-
 
 # Training model.
 model, dv = train(telco_train, y_train, c= c)
@@ -166,69 +91,8 @@ auc = roc_auc_score(y_test, y_pred)
 
 print(auc)
 
-
-# In[14]:
-
-
-customer = {
-    'customerid': '8879-zkjof',
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'no',
-    'dependents': 'no',
-    'tenure': 41,
-    'phoneservice': 'yes',
-    'multiplelines': 'no',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'yes',
-    'onlinebackup': 'no',
-    'deviceprotection': 'yes',
-    'techsupport': 'yes',
-    'streamingtv': 'yes',
-    'streamingmovies': 'yes',
-    'contract': 'one_year',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'bank_transfer_(automatic)',
-    'monthlycharges': 79.85,
-    'totalcharges': 3320.75
-}
-
-
-# In[15]:
-
-
-def predict_customer(customer, model, dv):
-    x = dv.transform(customer)
-    y_pred = model.predict_proba(x)[:, 1]
-    return y_pred[0].round(3)
-
-
-# In[16]:
-
-
-# Predicting a single customer
-result = predict_customer(customer, model, dv)
-
-# Result of prediction.
-result
-
-
-# In[17]:
-
-
-# Importing pickle package.
-import pickle
-
-
-# In[18]:
-
-
 # file tile
 model = f'model_C={c}.bin'
-
-
-# In[19]:
-
 
 # writting model to .bin file. 
 with open(model, 'wb') as f_out: 
